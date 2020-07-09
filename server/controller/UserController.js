1fb19ebdcf83734f4f7389f9c2fecd1dd7d7064c
Galih.H <zenilevh@gmail.com>
2020-07-10 04:12:01 +0700

    update email validator unique

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -6,12 +6,20 @@ const verificationToken = require('../helper/googleOauth')
 class UserController {
 
     static register(req, res, next) {
+        let email = req.body.email
         const form = {
             name: req.body.name,
-            email: req.body.email,
+            email: email,
             password: req.body.password
         };
-        User.create(form)
+        User.findOne({ where: { email } })
+            .then((emailCheck) => {
+                if (emailCheck) {
+                    return res.status(400).json('Email already Registered');
+                } else {
+                    return User.create(form);
+                }
+            })
             .then((dataUser) => {
                 return res.status(201).json(dataUser);
             }).catch((err) => {
@@ -28,9 +36,7 @@ class UserController {
                 }
             })
             if (!dataUser) {
-                console.log(dataUser);
                 next({ errCode: "INVALID_EMAIL", message: "invalid username and password" })
-
             } else {
                 const password = req.body.password;
                 if (checkPassword(password, dataUser.password)) {
@@ -49,42 +55,42 @@ class UserController {
         }
     }
 
-    static googleSignIn(req, res ,next){
+    static googleSignIn(req, res, next) {
         let google_token = req.headers.google_token
         let email = null
         let newUser = false
         console.log('<<<<<<<test login')
         verificationToken(google_token)
-        .then(payload => {
-            email = payload.email
-            return User.findOne({
-                where: {
-                    email
+            .then(payload => {
+                email = payload.email
+                return User.findOne({
+                    where: {
+                        email
+                    }
+                })
+            })
+            .then(user => {
+                if (user) {
+                    return user
+                } else {
+                    newUser = true
+                    return User.create({
+                        email,
+                        password: process.env.DEFAULT_GOOGLE_PASSWORD
+                    })
                 }
             })
-        })
-        .then(user => {
-            if(user){
-                return user
-            } else {
-                newUser = true
-                return User.create({
-                    email,
-                    password: process.env.DEFAULT_GOOGLE_PASSWORD
-                })
-            }
-        })
-        .then(user => {
-            let code = newUser ? 202 : 201
-            const token = encode({
-                id: user.id,
-                email: user.email
-            },process.env.SECRET)
-            res.status(code).json({token: token})
-        })
-        .catch(err => {
-            next(err)
-        })
+            .then(user => {
+                let code = newUser ? 202 : 201
+                const token = encode({
+                    id: user.id,
+                    email: user.email
+                }, process.env.SECRET)
+                res.status(code).json({ token: token })
+            })
+            .catch(err => {
+                next(err)
+            })
     }
 
 }
